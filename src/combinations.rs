da92895068af9a00596f04f5f944154cd3d949cf
LunaAmora <luna.mberry@gmail.com>
2025-11-08 19:05:02 -0300

    Split solutions

--- src/combinations.rs
+++ src/combinations.rs
@@ -1,7 +1,7 @@
 use super::Pair;
 use super::Sum;
 
-pub fn compute_solutions(sequence: &[usize; 12], pair_sums: &[Sum<Pair>]) -> Vec<[Pair; 12]> {
+pub fn compute_solutions(sequence: &[usize; 12], pair_sums: &[Sum<Pair>]) -> Vec<([Pair; 6], [Pair; 6])> {
     // Try all possible combinations of selecting one pair from each sum in the sequence
     let pair_options: [&Vec<Pair>; 12] =
         sequence.iter().map(|&sum_value| &pair_sums[sum_value - 3].0).collect::<Vec<&Vec<Pair>>>().try_into().unwrap();
@@ -10,7 +10,15 @@ pub fn compute_solutions(sequence: &[usize; 12], pair_sums: &[Sum<Pair>]) -> Vec
     let mut all_solutions = Vec::new();
     let mut selected_pairs = Vec::new();
     find_all_valid_combinations(&pair_options, 0, &mut selected_pairs, &mut all_solutions);
-    all_solutions
+
+    let mut results = Vec::new();
+
+    for pairs in all_solutions {
+        let splits = split_pairs_evenly(pairs);
+        results.extend(splits);
+    }
+
+    results
 }
 
 fn find_all_valid_combinations(
@@ -40,7 +48,7 @@ fn check_frequency_constraint(pairs: &[Pair]) -> bool {
     let mut digit_count = [0; 9];
 
     // Count occurrences of each digit
-    for &(a, b) in pairs {
+    for &[a, b] in pairs {
         digit_count[a - 1] += 1;
         digit_count[b - 1] += 1;
     }
@@ -60,4 +68,88 @@ fn check_frequency_constraint(pairs: &[Pair]) -> bool {
 
     // We need exactly 3 digits appearing 4 times, 6 digits appearing 2 times
     count_4 == 3 && count_2 == 6
+}
+
+/// Splits a Vec<Pair> into 2 groups of 6 pairs each, maintaining frequency constraints.
+/// Returns all valid ways to split the pairs.
+/// Each group should have: 3 digits appearing 2 times, 6 digits appearing once.
+pub fn split_pairs_evenly(pairs: [Pair; 12]) -> Vec<([Pair; 6], [Pair; 6])> {
+    let mut results = Vec::new();
+
+    // Generate all combinations of 6 items from 12 using bit manipulation
+    // We need combinations of 6 out of 12, which is C(12,6) = 924 combinations
+    // To avoid duplicates, we only consider masks where the first bit is set
+    // This ensures we only generate each unique split once
+    for mask in 0u32..(1u32 << 12) {
+        if mask.count_ones() == 6 && (mask & 1) == 1 {
+            let group1: [Pair; 6] = (0..12)
+                .filter(|&i| (mask & (1 << i)) != 0)
+                .map(|i| pairs[i])
+                .collect::<Vec<Pair>>()
+                .try_into()
+                .unwrap();
+
+            let group2: [Pair; 6] = (0..12)
+                .filter(|&i| (mask & (1 << i)) == 0)
+                .map(|i| pairs[i])
+                .collect::<Vec<Pair>>()
+                .try_into()
+                .unwrap();
+
+            // Check if both groups satisfy the constraint
+            if check_group_constraint(&group1) && check_group_constraint(&group2) {
+                results.push((group1, group2));
+            }
+        }
+    }
+
+    results
+}
+
+/// Checks if a group of 6 pairs satisfies the constraint:
+/// 3 digits appearing 2 times, 6 digits appearing once
+fn check_group_constraint(pairs: &[Pair; 6]) -> bool {
+    let mut digit_count = [0; 9];
+
+    // Count occurrences of each digit
+    for &[a, b] in pairs {
+        digit_count[a - 1] += 1;
+        digit_count[b - 1] += 1;
+    }
+
+    // Count how many digits appear exactly 2 times and exactly 1 time
+    let mut count_2 = 0;
+    let mut count_1 = 0;
+
+    for &count in &digit_count[0..9] {
+        match count {
+            2 => count_2 += 1,
+            1 => count_1 += 1,
+            0 => return false,
+            _ => return false,
+        }
+    }
+
+    // We need exactly 3 digits appearing 2 times, 6 digits appearing 1 time
+    count_2 == 3 && count_1 == 6
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_split_pairs_evenly() {
+        // 14 15 25 26 37 38 48 58 59 69 79 89
+        let pairs = [[1, 4], [1, 5], [2, 5], [2, 6], [3, 7], [3, 8], [4, 8], [5, 8], [5, 9], [6, 9], [7, 9], [8, 9]];
+
+        let splits = split_pairs_evenly(pairs);
+
+        println!("Found {} valid splits:", splits.len());
+        for (i, (group1, group2)) in splits.iter().enumerate() {
+            println!("Split {}:", i + 1);
+            println!("  Group 1: {:?}", group1);
+            println!("  Group 2: {:?}", group2);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,10 @@ fn main() {
     print_solutions(&pairs_sequence, &pair_sums);
 }
 
-fn get_triplet_map(triplet_sums: Vec<Sum<Triplet>>) -> IndexMap<Triplet, Vec<(Triplet, Triplet, Triplet)>> {
-    let mut res: IndexMap<_, Vec<(_, _, _)>> = IndexMap::new();
+type CornerTriplets = [Triplet; 3];
+
+fn get_triplet_map(triplet_sums: Vec<Sum<Triplet>>) -> IndexMap<Triplet, Vec<CornerTriplets>> {
+    let mut res: IndexMap<_, Vec<CornerTriplets>> = IndexMap::new();
 
     for i in 0..triplet_sums.len() {
         for j in (i + 1)..triplet_sums.len() {
@@ -37,9 +39,9 @@ fn get_triplet_map(triplet_sums: Vec<Sum<Triplet>>) -> IndexMap<Triplet, Vec<(Tr
                 }
 
                 let triplets = find_unique_triplets(&triplet_sums[i], &triplet_sums[j], &triplet_sums[k]);
-                for (a, b, c) in triplets {
-                    let e = (i + 6, j + 6, k + 6);
-                    res.entry(e).or_default().push((a, b, c));
+                for [a, b, c] in triplets {
+                    let e = [i + 6, j + 6, k + 6];
+                    res.entry(e).or_default().push([a, b, c]);
                 }
             }
         }
@@ -47,16 +49,16 @@ fn get_triplet_map(triplet_sums: Vec<Sum<Triplet>>) -> IndexMap<Triplet, Vec<(Tr
     res
 }
 
-fn find_unique_triplets(s1: &Sum<Triplet>, s2: &Sum<Triplet>, s3: &Sum<Triplet>) -> Vec<(Triplet, Triplet, Triplet)> {
+fn find_unique_triplets(s1: &Sum<Triplet>, s2: &Sum<Triplet>, s3: &Sum<Triplet>) -> Vec<CornerTriplets> {
     let mut res = vec![];
     for t1 in &s1.0 {
         for t2 in &s2.0 {
             for t3 in &s3.0 {
-                let mut digits = vec![t1.0, t1.1, t1.2, t2.0, t2.1, t2.2, t3.0, t3.1, t3.2];
+                let mut digits = vec![t1[0], t1[1], t1[2], t2[0], t2[1], t2[2], t3[0], t3[1], t3[2]];
                 digits.sort();
                 digits.dedup();
                 if digits.len() == 9 {
-                    res.push((*t1, *t2, *t3));
+                    res.push([*t1, *t2, *t3]);
                 }
             }
         }
@@ -64,22 +66,20 @@ fn find_unique_triplets(s1: &Sum<Triplet>, s2: &Sum<Triplet>, s3: &Sum<Triplet>)
     res
 }
 
-fn print_triple_map(triplet_map: &IndexMap<Triplet, Vec<(Triplet, Triplet, Triplet)>>) {
-    for ((s1, s2, s3), triplets) in triplet_map {
-        println!("({s1}, {s2}, {s3})");
-        for ((a1, a2, a3), (b1, b2, b3), (c1, c2, c3)) in triplets {
+fn print_triple_map(triplet_map: &IndexMap<Triplet, Vec<CornerTriplets>>) {
+    for (sum, triplets) in triplet_map {
+        println!("{:?}", sum);
+        for [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]] in triplets {
             println!(" {}{}{} {}{}{} {}{}{}", a1, a2, a3, b1, b2, b3, c1, c2, c3);
         }
         println!();
     }
 }
 
-fn get_pairs_sequence(
-    triplet_map: &IndexMap<Triplet, Vec<(Triplet, Triplet, Triplet)>>,
-) -> IndexMap<Triplet, [usize; 12]> {
+fn get_pairs_sequence(triplet_map: &IndexMap<Triplet, Vec<CornerTriplets>>) -> IndexMap<Triplet, [usize; 12]> {
     let mut pairs_sequence: IndexMap<Triplet, [_; 12]> = IndexMap::new();
 
-    for k @ (a, b, c) in triplet_map.keys().cloned() {
+    for k @ [a, b, c] in triplet_map.keys().cloned() {
         let mut sequence = [0; 12];
         let mut i = 11;
         let mut n = 17;
@@ -106,7 +106,7 @@ fn get_pairs_sequence(
     pairs_sequence
 }
 
-fn print_pairs_sequence(pairs_sequence: &IndexMap<(usize, usize, usize), [usize; 12]>) {
+fn print_pairs_sequence(pairs_sequence: &IndexMap<Triplet, [usize; 12]>) {
     for (key, sequence) in pairs_sequence {
         let key = format!("{:?}", key);
         println!("{:12} = {:2?}", key, sequence);
@@ -114,9 +114,9 @@ fn print_pairs_sequence(pairs_sequence: &IndexMap<(usize, usize, usize), [usize;
 }
 
 fn filter_triplets(
-    triplet_map: &mut IndexMap<Triplet, Vec<(Triplet, Triplet, Triplet)>>,
-    pairs_sequence: &IndexMap<(usize, usize, usize), [usize; 12]>,
-    pair_sums: &[Sum<(usize, usize)>],
+    triplet_map: &mut IndexMap<Triplet, Vec<CornerTriplets>>,
+    pairs_sequence: &IndexMap<Triplet, [usize; 12]>,
+    pair_sums: &[Sum<Pair>],
 ) {
     for (tripplet, seq) in pairs_sequence {
         let solutions = compute_solutions(seq, pair_sums);
@@ -126,26 +126,31 @@ fn filter_triplets(
     }
 }
 
-fn print_solutions(pairs_sequence: &IndexMap<(usize, usize, usize), [usize; 12]>, pair_sums: &[Sum<(usize, usize)>]) {
+fn print_solutions(pairs_sequence: &IndexMap<Triplet, [usize; 12]>, pair_sums: &[Sum<Pair>]) {
     for (tripplet, seq) in pairs_sequence {
         let solutions = compute_solutions(seq, pair_sums);
         if !solutions.is_empty() {
             println!(
-                "✓ Found {} valid combination(s) for triplet {:?} with sequence {:?}",
+                "✓ Found {} valid combination(s) for triplet {:?} with sum sequence {:?}",
                 solutions.len(),
                 tripplet,
                 seq
             );
             for (i, solution) in solutions.iter().enumerate() {
                 let mut digit_count = [0; 9];
-                for (a, b) in solution {
+                for [a, b] in solution.0 {
                     digit_count[a - 1] += 1;
                     digit_count[b - 1] += 1;
                 }
-                let duplicated: Vec<usize> = (0..9).filter(|&digit| digit_count[digit] == 4).map(|i| i + 1).collect();
-                println!("  Solution {:2} {:?}: {}", i + 1, duplicated, Sum(solution.to_vec()));
+                let duplicated: Vec<usize> = (0..9).filter(|&digit| digit_count[digit] == 2).map(|i| i + 1).collect();
+                println!(
+                    "  Solution {:3} {:?}: {} | {}",
+                    i + 1,
+                    duplicated,
+                    Sum(solution.0.to_vec()),
+                    Sum(solution.1.to_vec())
+                );
             }
         }
-        println!();
     }
 }
